193037ff78f812aa8752f85bb9a14791b1dd96e3
tsaichris <103312164+tsaichris@users.noreply.github.com>
2024-06-04 19:39:10 +0800

    Add files via upload

---
+++ UNet_modified.py
@@ -0,0 +1,112 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torchsummary import summary
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.conv(x)
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+    def forward(self, x):
+        return self.down(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=False):
+        super().__init__()
+        if bilinear:
+            self.up = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                    nn.Conv2d(in_channels, in_channels // 2, 1))
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, 2, stride=2)
+            
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.conv(x))
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super().__init__()
+        self.inc = DoubleConv(n_channels, 128)
+        self.down1 = Down(128, 256)
+        self.down2 = Down(256, 512)
+        self.down3 = Down(512, 1024)
+        self.down4 = Down(1024, 2048)
+        self.down5 = Down(2048, 4096)
+        self.up1 = Up(4096, 2048)
+        self.up2 = Up(2048, 1024)
+        self.up3 = Up(1024, 512)
+        self.up4 = Up(512, 256)
+        self.up5 = Up(256, 128)
+        self.outc = OutConv(128, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.down5(x5)
+        x = self.up1(x6, x5)
+        x = self.up2(x, x4)
+        x = self.up3(x, x3)
+        x = self.up4(x, x2)
+        x = self.up5(x, x1)
+        logits = self.outc(x)
+        return logits
+
+class DICE_BCE_Loss(nn.Module):
+    def __init__(self, smooth=1):
+        super().__init__()
+        self.smooth = smooth
+
+    def forward(self, logits, targets):
+        intersection = 2*(logits * targets).sum() + self.smooth
+        union = (logits + targets).sum() + self.smooth
+        dice_loss = 1. - intersection / union
+
+        loss = nn.BCELoss()
+        bce_loss = loss(logits, targets)
+
+        return dice_loss + bce_loss
+    
+def dice_coeff(logits, targets):
+    intersection = 2*(logits * targets).sum()
+    union = (logits + targets).sum()
+    if union == 0:
+        return 1
+    dice_coeff = intersection / union
+    return dice_coeff.item()
+model = UNet(4, 1)
+summary(model, input_size=(4, 428, 428)) 
